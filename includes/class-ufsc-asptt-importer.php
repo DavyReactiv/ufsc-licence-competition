8afa8b490647e4c535e212ac51b72ab766a2c8a4
DavyReactiv <davycteam@gmail.com>
2026-01-14 03:23:35 +0100

    Fix document format ordering on upsert

--- includes/class-ufsc-asptt-importer.php
+++ includes/class-ufsc-asptt-importer.php
@@ -654,12 +654,34 @@ class UFSC_ASPTT_Importer {
 			'updated_at'             => current_time( 'mysql' ),
 		);
 
-		$formats = array( '%d', '%s', '%s', '%d', '%s', '%s' );
+		$field_formats = array(
+			'licence_id'            => '%d',
+			'source'                => '%s',
+			'source_licence_number' => '%s',
+			'attachment_id'         => '%d',
+			'asptt_club_note'       => '%s',
+			'updated_at'            => '%s',
+			'source_created_at'     => '%s',
+			'imported_at'           => '%s',
+		);
+
+		$formats = array();
+		foreach ( array_keys( $data ) as $field ) {
+			if ( isset( $field_formats[ $field ] ) ) {
+				$formats[] = $field_formats[ $field ];
+			}
+		}
 
 		if ( $existing ) {
 			$wpdb->update( $table, $data, array( 'id' => (int) $existing ), $formats, array( '%d' ) );
 		} else {
 			$data['imported_at'] = current_time( 'mysql' );
+			$formats = array();
+			foreach ( array_keys( $data ) as $field ) {
+				if ( isset( $field_formats[ $field ] ) ) {
+					$formats[] = $field_formats[ $field ];
+				}
+			}
 			$wpdb->insert( $table, $data, $formats );
 		}
 	}
