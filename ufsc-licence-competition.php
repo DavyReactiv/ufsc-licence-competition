b9869c2ac747c0adfd959471fdbeb0c6b9e100c0
DavyReactiv <davycteam@gmail.com>
2026-08-23 23:45:41 +0200

    Register fighter number audit and event operations panel

--- ufsc-licence-competition.php
+++ ufsc-licence-competition.php
@@ -57,6 +57,7 @@ require_once UFSC_LC_DIR . 'includes/competitions/Services/CompetitionDuplicatio
 require_once UFSC_LC_DIR . 'includes/competitions/Services/DatabaseTransaction.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Services/AtomicOperationLock.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Services/FightResultPersistence.php';
+require_once UFSC_LC_DIR . 'includes/competitions/Services/FighterNumberAuditService.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Services/CompetitionIntegrityService.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Services/DatabaseSchemaGuard.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Services/Pancrace/PancraceReference2025.php';
@@ -65,6 +66,7 @@ require_once UFSC_LC_DIR . 'includes/competitions/Admin/Pages/EventProgram_Page.
 require_once UFSC_LC_DIR . 'includes/competitions/Admin/PremiumExperience.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Admin/EventLifecycleAdmin.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Admin/CompetitionRequirementsAdmin.php';
+require_once UFSC_LC_DIR . 'includes/competitions/Admin/EventOperationsPanel.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Admin/IntegrityHealthNotice.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Admin/ResultConcurrencyAdmin.php';
 require_once UFSC_LC_DIR . 'includes/competitions/Front/PremiumRegistrationExperience.php';
@@ -77,6 +79,7 @@ require_once UFSC_LC_DIR . 'includes/competitions/Front/ClubCompetitionPortal.ph
 \UFSC\Competitions\Admin\PremiumExperience::register();
 \UFSC\Competitions\Admin\EventLifecycleAdmin::register();
 \UFSC\Competitions\Admin\CompetitionRequirementsAdmin::register();
+\UFSC\Competitions\Admin\EventOperationsPanel::register();
 \UFSC\Competitions\Admin\IntegrityHealthNotice::register();
 \UFSC\Competitions\Admin\ResultConcurrencyAdmin::register();
 \UFSC\Competitions\Front\PremiumRegistrationExperience::register();
