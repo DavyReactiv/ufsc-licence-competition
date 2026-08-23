603022abd0cbc0fab5439be18e4a3159a963b668
DavyReactiv <davycteam@gmail.com>
2026-08-23 20:27:06 +0200

    Define plugin constants for static analysis

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( ! defined( 'UFSC_LC_DIR' ) ) {
+	define( 'UFSC_LC_DIR', __DIR__ . '/' );
+}
+if ( ! defined( 'UFSC_LC_URL' ) ) {
+	define( 'UFSC_LC_URL', 'https://example.invalid/wp-content/plugins/ufsc-licence-competition/' );
+}
