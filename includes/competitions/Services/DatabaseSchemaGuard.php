d396d84fea646e04aac360d489d58fa9f5adb89c
DavyReactiv <davycteam@gmail.com>
2026-08-24 11:34:51 +0200

    test(db): guard competition logs schema

--- includes/competitions/Services/DatabaseSchemaGuard.php
+++ includes/competitions/Services/DatabaseSchemaGuard.php
@@ -62,6 +62,11 @@ class DatabaseSchemaGuard {
 				'columns' => array( 'id', 'competition_id', 'category_id', 'fight_no', 'red_entry_id', 'blue_entry_id', 'status', 'updated_at', 'deleted_at' ),
 				'indexes' => array( 'PRIMARY', 'idx_competition_id', 'idx_status', 'idx_fight_no' ),
 			),
+			'logs' => array(
+				'table'   => Db::logs_table(),
+				'columns' => array( 'id', 'object_type', 'object_id', 'level', 'message', 'created_at' ),
+				'indexes' => array( 'PRIMARY', 'idx_object', 'idx_created_at' ),
+			),
 			'timing_profiles' => array(
 				'table'   => Db::timing_profiles_table(),
 				'columns' => array( 'id', 'name', 'round_duration', 'rounds', 'break_duration' ),
